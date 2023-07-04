83abeb63e5bdd7da66faa76b578f1fe4da165681
luquinhasCS <lucasdecarvalho669@gmail.com>
2023-07-04 09:33:45 -0300

    inicio do projeto mais pica do mundo

---
+++ projetobombeiros/src/App.js
@@ -0,0 +1,24 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://picsum.photos/200" className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Go to youtu
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
