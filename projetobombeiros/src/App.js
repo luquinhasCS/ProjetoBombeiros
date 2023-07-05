cc1184e990d2ec623cbc1b72a4dfa8b15aa11006
luquinhasCS <lucasdecarvalho669@gmail.com>
2023-07-05 09:39:02 -0300

    dá nao

--- projetobombeiros/src/App.js
+++ projetobombeiros/src/App.js
@@ -1,22 +1,10 @@
-
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="https://picsum.photos/200" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://www.youtube.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Go to youtu
-        </a>
-      </header>
+      
+      <input id="div" className="form-input" placeholder="Nome"/>
     </div>
   );
 }
